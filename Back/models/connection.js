0177c50a5184e5be1ee4f346a8848024fb03cfe3
Adrien Villermois <ad.villermois@gmail.com>
2023-09-19 15:57:18 +0200

    back

--- Back/models/connection.js
+++ Back/models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString ="mongodb+srv://admin:9bXOhAuXNkCT03tE@cluster1.qignvzz.mongodb.net/app-eduSign"
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
