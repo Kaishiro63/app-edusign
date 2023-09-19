c984d4e30c2fc7269a18d609ae4ae600f577463c
Adrien Villermois <ad.villermois@gmail.com>
2023-09-19 16:29:55 +0200

    delete back

--- Back/models/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectionString ="mongodb+srv://admin:9bXOhAuXNkCT03tE@cluster1.qignvzz.mongodb.net/app-eduSign"
-
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch(error => console.error(error));
