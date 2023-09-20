b68b8cd974134de44019ea9964cd1195309808bd
Guillaume Benard <guillaume.benard0@gmail.com>
2023-09-20 16:49:27 +0200

    maj profile tab

--- Front/src/screens/profilScreen.jsx
+++ Front/src/screens/profilScreen.jsx
@@ -1,11 +1,53 @@
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const ProfilScreen = () => {
-    return (
-        <View>
-            <Text>ProfilScreen</Text>
-        </View>
-    );
-}
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const handleGetUser = async () => {
+      console.log("test");
+      try {
+        const response = await fetch(
+          "https://app-edusign-back1.vercel.app/users/profile?uid=650ab8c16ea8d8449ae3be12"
+        );
+        const data = await response.json();
+        if (!data.result) {
+          console.log("erreur de fetch");
+          return;
+        } else {
+          setUser(data.allDataUser);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleGetUser();
+  }, []);
+
+
+
+  return (
+    <View style={screenStyles.container}>
+      <Text style={screenStyles.title}>{user.prenom} {user.nom.toUpperCase()}</Text>
+    </View>
+  )
+};
 
 export default ProfilScreen;
+
+const screenStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        paddingBottom: 120,
+        paddingHorizontal: 30,
+        justifyContent: 'center',
+
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
