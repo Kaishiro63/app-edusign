b14f1935ced82925f50d7bdcc603cf3a48e01074
Guillaume Benard <guillaume.benard0@gmail.com>
2023-09-20 10:20:24 +0200

    mainTabBar v1

--- Front/src/components/MainTabBar.jsx
+++ Front/src/components/MainTabBar.jsx
@@ -1,19 +1,57 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import LoginScreen from "../screens/LoginScreen";
 import HomeScreen from "../screens/HomeScreen";
 import DocumentsScreen from "../screens/DocumentsScreen";
 import StatsScreen from "../screens/StatsScreen";
 import ProfilScreen from "../screens/ProfilScreen";
+import CalendarScreen from "../screens/CalendarScreen";
 
 const Tab = createBottomTabNavigator();
 
 const MainTabBar = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarStyle: {
+                paddingTop: 5,
+            },
+            tabBarIcon: ({ color, size }) => {
+              let iconName = "";
+    
+              if (route.name === "Accueil") {
+                iconName = "home";
+                return <FontAwesome name={iconName} size={size} color={color} />;
+
+              } else if (route.name === "Statistiques") {
+                iconName = "stats-chart";
+                return <Ionicons name={iconName} size={size} color={color} />;
+
+              } else if (route.name === "Documents") {
+                iconName = "folder-open-outline";
+                return <Ionicons name={iconName} size={size} color={color} />;
+
+              } else if (route.name === "Calendrier") {
+                iconName = "calendar-outline";
+                return <Ionicons name={iconName} size={size} color={color} />;
+
+              } else if (route.name === "Profil") {
+                iconName = "user-circle-o";
+                return <FontAwesome name={iconName} size={size} color={color} />;
+
+              }
+
+            },
+            tabBarActiveTintColor: "#e3c022",
+            tabBarInactiveTintColor: "#8a8a8a",
+            headerShown: true,
+          })}>
             <Tab.Screen name="Login" component={LoginScreen} />
-            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Accueil" component={HomeScreen} />
+            <Tab.Screen name="Statistiques" component={StatsScreen} />
             <Tab.Screen name="Documents" component={DocumentsScreen} />
-            <Tab.Screen name="Stats" component={StatsScreen} />
+            <Tab.Screen name="Calendrier" component={CalendarScreen} />
             <Tab.Screen name="Profil" component={ProfilScreen} />
 
         </Tab.Navigator>
