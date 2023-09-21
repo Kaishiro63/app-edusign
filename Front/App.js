47466db9d5a1a61faa182d0b0133211ed31ce4b5
Quentin <quentin.gouttesoulard63@gmail.com>
2023-09-21 14:12:15 +0200

    merge conflict

--- Front/App.js
+++ Front/App.js
@@ -1,16 +1,8 @@
 
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import MainTabBar from "./src/components/MainTabBar";
-import HomeScreen from "./src/screens/HomeScreen";
-import LoginScreen from "./src/screens/LoginScreen";
 import user from "./reducers/user";
-import ScannerScreen from "./src/screens/ScannerScreen";
-import CustomHeader from "./src/components/CustomHeader";
-import JustifieScreen from "./src/screens/JustifieScreen";
-import { useSelector } from "react-redux";
 import { Route } from "./src/components/Route";
 
 const store = configureStore({
@@ -23,17 +15,7 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ header: (props) => <CustomHeader {...props} /> }}>
-          <Stack.Screen name="MainTab" component={MainTabBar} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Scan" component={ScannerScreen} />
-          <Stack.Screen name="Sign" component={SignScreen} />
-          <Stack.Screen name="Justifie" component={JustifieScreen} />
-          <Stack.Screen name="SingleCours" component={SingleCoursScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Route />
     </Provider>
   );
 }
