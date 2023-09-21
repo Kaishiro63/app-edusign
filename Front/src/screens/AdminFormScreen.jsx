8e4c8639022ede8acb4674007b18e55c32c5103d
Adrien Villermois <ad.villermois@gmail.com>
2023-09-21 10:37:52 +0200

    debut Admin Feature

---
+++ Front/src/screens/AdminFormScreen.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const AdminFormScreen = () => {
+    return (
+        <View>
+            <Text>AdminForm</Text>
+        </View>
+    );
+}
+
+export default AdminFormScreen;
---
+++ Front/src/screens/QrCodeScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+
+const QrCodeScreen = () => {
+    return (
+        <View>
+            <Text>QR_CODE_SCREEN</Text>
+        </View>
+    );
+}
+
+
+
+
+export default QrCodeScreen;
