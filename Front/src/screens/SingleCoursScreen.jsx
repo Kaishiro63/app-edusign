b22fe24b63e3b6b4c918e14e373ab9d5ed5bedae
Quentin <quentin.gouttesoulard63@gmail.com>
2023-09-21 15:07:30 +0200

    add filtre cours

--- Front/src/screens/SingleCoursScreen.jsx
+++ Front/src/screens/SingleCoursScreen.jsx
@@ -8,16 +8,59 @@ const SingleCoursScreen = ({ route, navigation }) => {
     navigation.navigate('Sign');
   };
 
+  const formatDateToHourMinute = (dateString) => {
+    const date = new Date(dateString);
+    const hour = date.getHours().toString().padStart(2, "0");
+    const minute = date.getMinutes().toString().padStart(2, "0");
+    return `${hour}:${minute}`;
+  };
+
+  const formatDateToMMDDYY = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear().toString().slice(-2);
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    return `${day}/${month}/${year}`;
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableOpacity onPress={() => navigation.goBack()}>
         <Text>Retour</Text>
       </TouchableOpacity>
-      <Text>Titre : {titre}</Text>
-      <Text>Start : {start}</Text>
-      <Text>End : {end}</Text>
-      <Text>End : {intervenant}</Text>
-      <Text>End : {salle}</Text>
+      <View style={styles.card}>
+        <Text style={styles.reveil}>⏰</Text>
+        <View style={styles.infoContainer}>
+        <Text style={styles.title}>{formatDateToHourMinute(start)} - <Text style={styles.title}>{formatDateToHourMinute(end)}</Text></Text>
+          <View style={styles.timeContainer}>
+            <Text style={styles.hourStart}>{formatDateToMMDDYY(start)}</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <Text style={styles.salle}>🏠</Text>
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>{salle}</Text>
+          <View style={styles.timeContainer}>
+            <Text style={styles.hourStart}>Salle</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <Text style={styles.intervenant}>👱‍♂️</Text>
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>{intervenant}</Text>
+          <View style={styles.timeContainer}>
+            <Text style={styles.hourStart}>Intervenant</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.classe}>
+        <Text style={styles.title}>Description</Text>
+        <Text style={styles.hourStart}>{titre}</Text>
+      </View>
       <View style={styles.flex}>
         <TouchableOpacity
           style={styles.btn}
@@ -31,6 +74,68 @@ const SingleCoursScreen = ({ route, navigation }) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 30,
+    paddingTop: 40,
+    backgroundColor: "#FFF"
+  },
+  card: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+    marginTop: 20
+  },
+  col: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#FFF",
+  },
+  reveil: {
+    fontSize: 28,
+    backgroundColor: "#F6E2D9",
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    marginRight: 10
+  },
+  intervenant: {
+    fontSize: 28,
+    backgroundColor: "#FCF1D1",
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    marginRight: 10
+  },
+  salle: {
+    fontSize: 28,
+    backgroundColor: "#FCF1D1",
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    marginRight: 10
+  },
+  classe: {
+    marginTop: 20
+  },
+  infoContainer: {
+    flex: 1,
+  },
+  timeContainer: {
+    flexDirection: 'row',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  hourStart: {
+    marginRight: 10,
+    color: '#AAAAAA',
+  },
+  hourEnd: {
+    color: '#AAAAAA',
+  },
   flex: {
     display: "flex",
     justifyContent: "center",
