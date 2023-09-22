f1d8c61c7ee4ea59d7c1fd44f2b6f031c8619c9f
Guillaume Benard <guillaume.benard0@gmail.com>
2023-09-22 13:54:47 +0200

    ajouts qr code etc

--- Front/src/screens/JustifieScreen.jsx
+++ Front/src/screens/JustifieScreen.jsx
@@ -1,21 +1,40 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Alert, StyleSheet, TouchableOpacity } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const JustifieScreen = () => {
   const [selectedOption, setSelectedOption] = useState(null);
-  const [dateDebut, setDateDebut] = useState('');
-  const [dateFin, setDateFin] = useState('');
-  const [document, setDocument] = useState(null);
-  const [checkBox, setCheckBox] = useState(false);
+  const [dateDebut, setDateDebut] = useState(new Date());
+  const [dateFin, setDateFin] = useState(new Date());
+  const [showStartDatePicker, setShowStartDatePicker] = useState(false);
+  const [showEndDatePicker, setShowEndDatePicker] = useState(false);
+  const [heureSelectionnee, setHeureSelectionnee] = useState(new Date().toLocaleTimeString());
 
-  const pickerRef = useRef(null);
+
+  const handleStartDateChange = (event, selectedDate) => {
+    setShowStartDatePicker(false);
+    if (selectedDate !== undefined) {
+      setDateDebut(selectedDate);
+      setHeureSelectionnee(selectedDate.toLocaleTimeString());
+    }
+  };
+  
+  const handleEndDateChange = (event, selectedDate) => {
+    setShowEndDatePicker(false);
+    if (selectedDate !== undefined) {
+      setDateFin(selectedDate);
+      setHeureSelectionnee(selectedDate.toLocaleTimeString());
+    }
+  };
 
   const handlePickerPress = () => {
-    if (pickerRef.current) {
-      pickerRef.current.togglePicker();
-    }
+    setShowStartDatePicker(true);
+  };
+
+  const handleEndDatePickerPress = () => {
+    setShowEndDatePicker(true);
   };
 
   const handleSelectChange = (value) => {
@@ -25,9 +44,70 @@ const JustifieScreen = () => {
   const handleSubmit = () => {
     Alert.alert(
       'Formulaire soumis',
-      `Option sélectionnée: ${selectedOption}\nInput 1: ${input1}\nInput 2: ${input2}\nDocument: ${document}\nCase à cocher: ${checkBox}`
+      `Option sélectionnée: ${selectedOption}\nDate de début: ${dateDebut.toDateString()}\nDate de fin: ${dateFin.toDateString()}`
     );
   };
+
+  console.log("dateDeDebut:", dateDebut);
+
+  return (
+    <View style={screenStyles.container}>
+      <Text style={screenStyles.title}>Justifier une absence</Text>
+      <Text style={screenStyles.text}>La demande de justification sera envoyée à un administrateur pour une validation.</Text>
+      
+      <View style={screenStyles.dateContainer}>
+      <TouchableOpacity onPress={handlePickerPress}>
+        <Text style={screenStyles.text}>Date de début : {dateDebut.toLocaleDateString()}, {heureSelectionnee}</Text>
+      </TouchableOpacity>
+      {showStartDatePicker && (
+        <DateTimePicker
+          value={dateDebut}
+          mode="datetime"
+          dateFormat="day month year"
+          display="compact"
+          format="DD/MM/YYYY"
+          onChange={handleStartDateChange}
+        />
+      )}
+
+      </View>
+      <View style={screenStyles.dateContainer}>
+      <TouchableOpacity onPress={handleEndDatePickerPress}>
+        <Text style={screenStyles.text}>Date de fin : {dateFin.toLocaleDateString()}, {heureSelectionnee}</Text>
+      </TouchableOpacity>
+      {showEndDatePicker && (
+        <DateTimePicker
+          value={dateFin}
+          mode="datetime"
+          display="compact"
+          dateFormat="day month year"
+          onChange={handleEndDateChange}
+        />
+      )}
+
+      </View>
+      <TouchableOpacity onPress={handlePickerPress}>
+        <Text style={screenStyles.text}>Justification</Text>
+        <View style={screenStyles.pickerContainer}>
+          <RNPickerSelect
+            placeholder={{ label: 'Sélectionner une justification', value: null }}
+            onValueChange={handleSelectChange}
+            items={[
+              { label: 'Option 1', value: 'option1' },
+              { label: 'Option 2', value: 'option2' },
+            ]}
+            value={selectedOption}
+          />
+          <Icon name="arrow-drop-down" size={24} color="black" style={screenStyles.pickerIcon} />
+        </View>
+      </TouchableOpacity>
+      {/* ... rest of the components */}
+      <Button title="Soumettre" onPress={handleSubmit} />
+    </View>
+  );
+};
+
+export default JustifieScreen;
 
   const screenStyles = StyleSheet.create({
     container: {
@@ -60,56 +140,13 @@ const JustifieScreen = () => {
       marginLeft: 10,
       color: '#ccc',
     },
-    pickerItem: {
-      fontSize: 16,
-      color: '#ccc',
-      textAlign: 'center',
-    },
+    dateContainer:{
+      backgroundColor: 'red',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderBottomColor: '#ccc',
+      borderBottomWidth: 2,
+    }
   });
 
-  return (
-    <View style={screenStyles.container}>
-      <Text style={screenStyles.title}>Justifier une absence</Text>
-      <Text style={screenStyles.text}>La demande de justification sera envoyée à un administrateur pour une validation.</Text>
-      <TouchableOpacity onPress={handlePickerPress}>
-        <Text style={screenStyles.text}>Justification</Text>
-        <View style={screenStyles.pickerContainer}>
-          <RNPickerSelect
-            ref={pickerRef}
-            placeholder={{ label: 'Sélectionner une justification', value: null }}
-            onValueChange={handleSelectChange}
-            items={[
-              { label: 'Option 1', value: 'option1' },
-              { label: 'Option 2', value: 'option2' },
-            ]}
-            value={selectedOption}
-            style={{
-              inputIOS: screenStyles.pickerItem,
-              inputAndroid: screenStyles.pickerItem,
-            }}
-          />
-          <Icon name="arrow-drop-down" size={24} color="black" style={screenStyles.pickerIcon} />
-        </View>
-      </TouchableOpacity>
-      <TextInput
-        value={dateDebut}
-        onChangeText={(text) => setDateDebut(text)}
-        placeholder="Date de début"
-      />
-      <TextInput
-        value={dateFin}
-        onChangeText={(text) => setDateFin(text)}
-        placeholder="Date de fin"
-      />
-      <Button
-        title="Sélectionner un document PDF"
-        onPress={() => {
-          // Implement this
-        }}
-      />
-      <Button title="Soumettre" onPress={handleSubmit} />
-    </View>
-  );
-};
 
-export default JustifieScreen;
